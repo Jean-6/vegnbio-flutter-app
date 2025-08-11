bdb9bbc66e5168233d22c1febe855cad5c45d5db
Jean <oka.jeanjaures@gmail.com>
2025-08-11 17:11:29 +0200

    customer dash

---
+++ lib/presentation/screen/demo/customer_dash.dart
@@ -0,0 +1,142 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vegnbio/presentation/screen/customer/events_screen.dart';
+import 'package:vegnbio/presentation/screen/customer/menus_screen.dart';
+
+import '../../widget/content_card.dart';
+
+class CustomerDash extends StatelessWidget {
+  const CustomerDash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stylish Dashboard',
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primarySwatch: Colors.blue,
+      ),
+      home: CustomerDashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class CustomerDashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Member"),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Messages"),
+          BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: "My"),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            Text(
+              "Recommend",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                gradient: LinearGradient(
+                  colors: [Colors.blue.shade800, Colors.blue.shade400],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Smart bitcoin mining",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Row(
+                    children: [
+                      Icon(Icons.layers, color: Colors.white),
+                      SizedBox(width: 8),
+                      Text("PRODUCTS", style: TextStyle(color: Colors.white)),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                _iconBox(Icons.school, "Communauté",(){
+
+                }),
+                _iconBox(Icons.menu_book, "Reservations", (){
+
+                }),
+                _iconBox(Icons.category, "Menus", (){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => MenusScreen()),
+                  );
+                }),
+                _iconBox(Icons.card_giftcard, "Evenements", (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => EventsScreen()),
+                  );
+                }),
+              ],
+            ),
+            SizedBox(height: 30),
+            Text("Newest", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            SizedBox(height: 16),
+            ContentCard(
+                title: "Notre Dame de Paris",
+                desc: "You can try to write a letter to yourself in the future.",
+                rating: "4.9",
+                imagePath: 'assets/images/logo.png',
+                onTap: (){})
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _iconBox(IconData icon, String label, VoidCallback onTap) {
+    return InkWell(
+      onTap:onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color:Colors.blue.shade100,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Icon(icon,size: 30,color: Colors.blue),
+          ),
+          SizedBox(height:8),
+          Text(label)
+        ],
+      )
+    );
+  }
+}
